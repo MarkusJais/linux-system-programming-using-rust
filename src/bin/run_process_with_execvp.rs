b7593e185de4e3a606c583b12e8395d27efc37e9
Markus Jais <markusjais@gmx.de>
2015-10-10 16:51:24 +0200

    added example for execvp

---
+++ src/bin/run_process_with_execvp.rs
@@ -0,0 +1,46 @@
+extern crate nix;
+
+use nix::unistd::{fork, getpid, getppid, execvp};
+use nix::unistd::Fork::{Parent, Child};
+use nix::sys::wait::waitpid;
+use nix::sys::wait::WaitStatus;
+use std::ffi::CString;
+
+
+
+fn main() {
+    let pid = fork();
+    match pid {
+        Ok(Child) => {
+            let command =  &CString::new("/bin/cat").unwrap();
+            let arguments =
+                    &[
+                    CString::new(b"/bin/cat".as_ref()).unwrap(),
+                    CString::new(b"/tmp/file.txt".as_ref()).unwrap(),
+                    CString::new(b"-n".as_ref()).unwrap(),
+                    ];
+            execvp(command, arguments).unwrap();
+        }
+        Ok(Parent(child_pid)) => {
+
+            println!("in parent process with pid: {} and child pid:{}", getpid(), child_pid);
+
+            let wait_status = waitpid(child_pid, None);
+            match wait_status {
+                // assert that waitpid returned correct status and the pid is the one of the child
+                Ok(WaitStatus::Exited(pid, status)) =>  {
+                    println!("child process with pid {} exited with status: {}", pid, status);
+                },
+
+                // panic, must never happen
+                Ok(_) => panic!("Child still alive, should never happen"),
+
+                // panic, waitpid should never fail
+                Err(_) => panic!("Error: waitpid Failed")
+            }
+
+        },
+        // panic, fork should never fail unless there is a serious problem with the OS
+        Err(_) => panic!("Error: Fork Failed")
+    }
+}
